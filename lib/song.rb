ace8f63c82b7b88a47c14ee31beddbc691a31ce9
Jason Vance <jbvance@gmail.com>
2017-11-28 20:49:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,13 +5,13 @@ class Song
   def initialize(name)
     @name = name
   end
-  
+
   def self.new_by_fileName(filename)
     array = filename.split(" - ")
     name = array[1]
     artist = array[0]
     genre = array[2]
-    
+
     song  = self.new(filename)
     song.name = name
     song.artist = Artist.new(artist)
